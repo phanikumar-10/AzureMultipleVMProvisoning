4265c55ce00e6e27d71bd269cabdde00dcc932f1
phanikumar-10 <111853553+phanikumar-10@users.noreply.github.com>
2022-09-08 17:34:34 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,28 +2,28 @@ provider "azurerm" {
   features {}
 }
 #vnet config
-resource "azurerm_virtual_network" "example" {
-  name                = "example-network"
+resource "azurerm_virtual_network" "demo" {
+  name                = "demo-network"
   address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
 }
 
-resource "azurerm_subnet" "example" {
+resource "azurerm_subnet" "demo" {
   name                 = "internal"
-  resource_group_name  = azurerm_resource_group.example.name
-  virtual_network_name = azurerm_virtual_network.example.name
+  resource_group_name  = azurerm_resource_group.demo.name
+  virtual_network_name = azurerm_virtual_network.demo.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "example" {
-  name                = "example-nic"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+resource "azurerm_network_interface" "demo" {
+  name                = "demo-nic"
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.example.id
+    subnet_id                     = azurerm_subnet.demo.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id = azurerm_public_ip.public_ip.id
   }
@@ -31,8 +31,8 @@ resource "azurerm_network_interface" "example" {
 #security Group config
 resource "azurerm_network_security_group" "nsg" {
   name                = "ssh_nsg"
-  location            = azurerm_resource_group.example.location
-  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.demo.location
+  resource_group_name = azurerm_resource_group.demo.name
 
   security_rule {
     name                       = "allow_ssh_sg"
@@ -48,31 +48,32 @@ resource "azurerm_network_security_group" "nsg" {
 }
 
 resource "azurerm_network_interface_security_group_association" "association" {
-  network_interface_id      = azurerm_network_interface.example.id
+  network_interface_id      = azurerm_network_interface.demo.id
   network_security_group_id = azurerm_network_security_group.nsg.id
 }
 #ip config
 resource "azurerm_public_ip" "public_ip" {
   name                = "vm_public_ip"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_resource_group.demo.location
   allocation_method   = "Dynamic"
 }
 #vm config
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "demo" {
+  name     = "demo-resources"
   location = "Central India"
 }
 
-resource "azurerm_linux_virtual_machine" "example" {
-  name                = "example-machine"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+resource "azurerm_linux_virtual_machine" "demo" {
+  count               =var.vm_count
+  name                = "demo-machine-${count.index}"
+  resource_group_name = azurerm_resource_group.demo.name
+  location            = azurerm_resource_group.demo.location
   size                = "Standard_B1s"
   admin_username      = "adminuser"
 
   network_interface_ids = [
-    azurerm_network_interface.example.id,
+    azurerm_network_interface.demo.id,
   ]
 
   admin_ssh_key {
@@ -93,5 +94,4 @@ resource "azurerm_linux_virtual_machine" "example" {
   }
 
 }
-
 
