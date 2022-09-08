6aa372c4e5430f8c170cdcfcc9f3af7a54073b08
phanikumar-10 <111853553+phanikumar-10@users.noreply.github.com>
2022-09-08 17:49:16 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -78,7 +78,7 @@ resource "azurerm_linux_virtual_machine" "demo" {
 
   admin_ssh_key {
     username   = "adminuser"
-    public_key = file("~/.ssh/id_rsa.pub")
+    #public_key = file("~/.ssh/id_rsa.pub")
   }
 
   os_disk {
